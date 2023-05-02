74a91e05008496374110b39469bf7d08823b26bf
Semakin Grigory1991 <semakingrigory@yandex.ru>
2023-05-02 15:46:14 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("Hello, Kotlin!")
+    println("This is my first HomeWork!")
+
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
